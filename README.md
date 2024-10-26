eb6c7a8a046372321223396f3e76eb84b1efa64f
Jason Gillis <gillisj@gmail.com>
2024-10-26 06:49:46 +0100

    Good progress.

--- README.md
+++ README.md
@@ -3,8 +3,38 @@
 ## Components
 
 1. Raspberry Pi
+1. Cheap USB Audio device to provide microphone
 1. Dig-Uno
 1. 5v individually addressable LED string
 1. Several battery packs
 1. Buttons
 1. Pyle PWMA50B portable PA
+
+## System Setup
+
+1. Installs:
+   ```sudo apt update
+   sudo apt dist-upgrade
+   sudo apt install sox libsox-fmt-mp3 alsa-tools alsaplayer-text
+   ```
+
+1. Configure system to use USB Audio as default via `raspi-config`
+
+1. Enable SSH
+
+1. Install `git` certifcates to talk to internal Git repo properly
+
+## Button functions
+
+| Button | Press Type | Action |
+|---|---|---|
+| Red | Short | Reset to breathe effect |
+| Blue | Short | Play/Stop Mr. Roboto |
+| Left White | Short | Play/Stop walking sound |
+| Green | Short | Play/Stop Thriller |
+| Yellow | Short | Activate/Deactivate voice changes |
+| Black | Short | Play/Stop fart sounds |
+| Right White | Short | Play/Stop thinking sound |
+| Right White | Long Press | Turn Headlights on |
+
+At start-up, only the red button will be activated.  This allows for a wait until the WLED box is booted and available.  Press the red button to initialize the lights and enable the rest of the buttons.
--- button_reader.py
+++ button_reader.py
@@ -1,46 +1,260 @@
 #!env python3
 
+from enum import Enum
+
+from pprint import pprint
 from gpiozero import Button
+
+from random import randrange
 
 from signal import pause
 
+import requests
+import json
+
 import subprocess
+import os
+import signal
 import time
 import sys
 from functools import partial
 
+# Add a property to Button
+Button.was_held = False
+
 MR_ROBOTO_COMMAND=['play', '-q', '/home/jgillis/audio/Mr. Roboto.mp3']
-ROBOT_VOICE_COMMAND=['play', '-q', '|rec ']
-RED_BUTTON_GPIO=18
-YELLOW_BUTTON_GPIO=24
-BLACK_BUTTON_GPIO=11
+THRILLER_COMMAND=['play', '-q', '/home/jgillis/audio/Thriller.mp3']
+WALKING_COMMAND=['play', '-q', '/home/jgillis/audio/walking.mp3', 'tempo', '0.75', 'repeat', '10']
+THINKING_COMMAND=['play', '-q', '/home/jgillis/audio/thinking.mp3', 'repeat']
+UNMUTE_MIC_COMMAND=['amixer', '-q', '-c', '1', 'sset', 'Mic', 'on']
+MUTE_MIC_COMMAND=['amixer', '-q', '-c', '1', 'sset', 'Mic', 'off']
+TALKING_COMMAND=['play', '-q', '|rec -d gain 30 band 1.2k 1.5k highpass 20 compand .1,.2 -inf,-30.1,-inf,-30,-30 0 -90 .1 pitch -350 equalizer 100 50 12 equalizer 900 50 -46 equalizer 2500 50 12 treble 0.6 phaser 0.6 0.66 3 0.6 2 -t bass -7 overdrive 10']
+def FARTING_COMMAND():
+    return ['play', '-q', f'/home/jgillis/audio/fart{randrange(1,5)}.ogg']
+
+
+GPIO_3=3
+GPIO_4=4
+GPIO_14=14
+GPIO_15=15
+GPIO_18=18
+GPIO_17=17
+GPIO_27=27
+
+RED_BUTTON      = { 'color': 'red', 'gpio': GPIO_14 }
+BLUE_BUTTON     = { 'color': 'blue', 'gpio': GPIO_18 }
+R_WHITE_BUTTON  = { 'color': 'r_white', 'gpio': GPIO_17 }
+GREEN_BUTTON    = { 'color': 'green', 'gpio': GPIO_27 }
+
+YELLOW_BUTTON   = { 'color': 'yellow', 'gpio': GPIO_3 }
+BLACK_BUTTON    = { 'color': 'black', 'gpio': GPIO_4 }
+L_WHITE_BUTTON  = { 'color': 'l_white', 'gpio': GPIO_15 }
+
+ALL_BUTTONS = [ RED_BUTTON, BLUE_BUTTON, R_WHITE_BUTTON,
+                GREEN_BUTTON, YELLOW_BUTTON, BLACK_BUTTON,
+                L_WHITE_BUTTON ]
+
+class LightBoard:
+
+    FIELD_BREATHE = {"id":4,"fx":2,"sx":52,"ix": 128, "bri":255,
+                     "pal":0,"col":[[255,0,0],[0,0,0],[85,0,255]]}
+    FIELD_MUSIC = {"id":4,"fx":48,"sx":66,"ix":192,"bri":128,
+                   "pal":5,"col":[[255,0,0],[255,166,0],[0,255,21]]}
+    FIELD_WALKING = {}
+    FIELD_THINKING = {}
+
+    HEADLIGHTS = { 'transition': 1, 'bri': 128,
+                   'seg': [
+                       {'id': 0, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 1, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 2, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 3, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 4, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 5, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 6, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]},
+                       {'id': 7, 'fx': 0, 'sx': 128, 'ix': 128, 'bri': 128,
+                        'col': [[255,255,255],[0,0,0],[85,0,255]]}]
+                  }
+
+    ALL_OFF = {"transition": 1,
+               "seg":[{"id": 0,"on": False},
+                      {"id": 1,"on": False},
+                      {"id": 2,"on": False},
+                      {"id": 3,"on": False},
+                      {"id": 5,"on": False},
+                      {"id": 6,"on": False},
+                      {"id": 7,"on": False}]}
+
+    color_id_map = {
+        'red': 0,
+        'blue': 1,
+        'r_white': 2,
+        'green': 3,
+        'l_white': 5,
+        'black': 6,
+        'yellow': 7
+    }
+
+    button_lights = [
+        { 'id': 0, 'on': False, 'col': [[255,25,25]], 'fx': 0  },
+        { 'id': 1, 'on': False, 'col': [[25,25,255]], 'fx': 0  },
+        { 'id': 2, 'on': False, 'col': [[255,255,255]], 'fx': 0  },
+        { 'id': 3, 'on': False, 'col': [[25,255,25]], 'fx': 0  },
+        { 'id': 5, 'on': False, 'col': [[255,255,255]], 'fx': 0  },
+        { 'id': 6, 'on': False, 'col': [[25,25,25]], 'fx': 0  },
+        { 'id': 7, 'on': False, 'col': [[255,255,25]], 'fx': 0  }
+    ]
+
+    field  = {'id': 4, 'fx': 2, 'sx': 128, 'ix': 128,
+              'bri':64, 'on': False, 'pal': 0,
+              'col': [[255,0,0],[0,0,0],[85,0,255]]}
+
+
+    def __init__(self, url_base: str, brightness: int = 16):
+        self.url_base = url_base
+        self.brightness = brightness
+        headers = { 'Content-Type': 'application/json' }
+        request_data = { "ps": 6 }
+        r = requests.post(self.url_base, json=request_data, headers=headers)
+        if r.status_code == 200:
+            print(f'Initialized light board OK:  {r.status_code}')
+        else:
+            print(f'Problem with light board init:  {r.status_code}')
+
+    def color_switch(self, color: str, state: bool):
+        """Switch the requested light to be on or off.  The other
+        lights should be the opposite state."""
+        print(f'color = {color} , id = {self.color_id_map[color]}')
+        if color in self.color_id_map:
+            for idx in range(0,7):
+                if self.button_lights[idx]['id'] == self.color_id_map[color]:
+                    self.button_lights[idx]['on'] = state
+                else:
+                    self.button_lights[idx]['on'] = not state
+        # pprint(self.button_lights)
+
+    def set_field(self, new_field):
+        self.field = new_field
+
+    def color_on(self, color: str):
+        self.color_switch(color, True)
+
+    def color_off(self, color: str):
+        self.color_switch(color, False)
+
+    def headlights(self):
+        self.push_state(LightBoard.HEADLIGHTS)
+
+    def push_state(self, request_data: dict = {}):
+        headers = { 'Content-Type': 'application/json' }
+        if request_data == {}:
+            request_data = { "transition": 1,
+                             'bri': self.brightness,
+                             "seg": self.button_lights + [ self.field ] }
+        r = requests.post(self.url_base, json=request_data, headers=headers)
+
+        if r.status_code != 200:
+            print(f'Error in POST {r.status_code}:  {r.text}')
+
+
+class AudioState(Enum):
+    STOPPED = 0
+    WALKING = 1
+    THINKING = 2
+    MR_ROBOTO = 3
+    THRILLER = 4
+    CONFUSED = 5
+    TALKING = 6
+    FARTING = 7
 
 class Robocalypse:
 
     def __init__(self,
-                 red_button_gpio: int = 18,
-                 yellow_button_gpio: int = 24):
-        self.red_button_gpio = red_button_gpio
-        self.yellow_button_gpio = yellow_button_gpio
+                 gpios: list[dict] = []):
+        """Initialize the system.  gpios contains information for each
+        button containing the color and GPIO pin."""
         self.subproc = None
+        self.audio_state = AudioState.STOPPED
+
         self.buttons: dict[str, Button] = {}
 
-        self.init_red_button()
-        self.init_yellow_button()
+        self.gpios = gpios
+        self.init_buttons()
 
-    def init_red_button(self):
-        print('Initializing the red button')
-        self.buttons['red'] = Button(self.red_button_gpio, pull_up=True,
-                                     hold_repeat=False, hold_time=0.25)
-        self.buttons['red'].when_held = self.handle_red_button
-        print('Done init')
+        # The red button is the start button.  Wait for this press to
+        # allow the WLED to boot up.  Once pressed, the rest of the
+        # buttons are activated.
+        self.buttons['red'].when_released = self.startup_button
 
-    def init_yellow_button(self):
-        print('Initializing the yellow button')
-        self.buttons['yellow'] = Button(self.yellow_button_gpio, pull_up=True,
-                                     hold_repeat=False, hold_time=0.25)
-        self.buttons['yellow'].when_held = self.handle_yellow_button
-        print('Done init')
+    def startup_button(self):
+        """Initialize the light board and set up the buttons."""
+        self.light_board = LightBoard('http://172.16.6.171/json/state')
+
+        # Activate all the buttons
+        for button in self.buttons:
+            # self.buttons[button].when_held = self.handle_button
+            self.buttons[button].when_released = self.handle_button
+            self.buttons[button].when_held = self.handle_held
+
+    def init_buttons(self):
+        """Initialize the buttons using GPIOs on the Raspberry Pi"""
+        print('Initializing buttons:')
+        for gpio in self.gpios:
+            print(f' - Button for GPIO {gpio}')
+            #new_button = Button(gpio['gpio'], pull_up=True,
+            #                    hold_repeat=False, hold_time=0.125)
+            new_button = Button(gpio['gpio'], pull_up=True, hold_time=2.0)
+            # new_button.when_held = self.handle_button
+            # new_button.when_released = self.released
+            self.buttons[gpio['color']] = new_button
+        print('Done.')
+
+    def released(self, button: Button):
+        print(f'Button.held_time = {button.held_time}')
+
+    def handle_held(self, button: Button):
+        button_info = list(filter(lambda x: x['gpio'] == button.pin.number,
+                                  self.gpios))[0]
+
+        self.buttons[button_info['color']].was_held = True
+
+
+    def handle_button(self, button: Button):
+        button_info = list(filter(lambda x: x['gpio'] == button.pin.number,
+                                  self.gpios))[0]
+        long_press = self.buttons[button_info['color']].was_held
+
+        # print(f'\n+++ Received button press on GPIO {button_info["color"]} - {button_info["gpio"]}.')
+
+        self.light_board.color_on(button_info['color'])
+        self.light_board.push_state()
+
+        if button_info["color"] == "red":
+            self.handle_red_button()
+        elif button_info["color"] == 'blue':
+            self.handle_blue_button()
+        elif button_info["color"] == 'r_white':
+            self.handle_r_white_button()
+        elif button_info["color"] == 'l_white':
+            if long_press:
+                self.headlights()
+            else:
+                self.handle_l_white_button()
+        elif button_info["color"] == 'green':
+            self.handle_green_button()
+        elif button_info["color"] == 'black':
+            self.handle_black_button()
+        elif button_info["color"] == 'yellow':
+            self.handle_yellow_button()
+
+
 
     def go(self):
         while True:
@@ -48,39 +262,115 @@ class Robocalypse:
             print('.', end='')
             sys.stdout.flush()
 
+    def check_process(self) -> AudioState:
+        """Check the status of the command and update the audio state"""
+        if self.subproc is not None and self.subproc.poll() is not None:
+            self.audio_state = AudioState.STOPPED
+
+        return self.audio_state
+
     def start_process(self, command: list[str]):
-        self.subproc = subprocess.Popen(command)
+        """Launch the process specified by command and store the
+        popen information in subproc."""
+        self.subproc = subprocess.Popen(command,
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
 
     def terminate_process(self):
         # Only terminate if the process is still running.  This is
         # determined by poll()ing to see if a return value is present.
         if self.subproc.poll() is None:
+            # Just calling terminate on the process didn't work when
+            # talking command was in use.  This just caused the top
+            # level play command to become a zombie and the rec
+            # process was still going.  Sending a CTRL-C to the
+            # process via stdin seems to fix that.
+            self.subproc.stdin.write('\x03'.encode())
+            self.subproc.stdin.flush()
             self.subproc.terminate()
             self.subproc.wait()
+            # print('Done')
+
+        subprocess.run(MUTE_MIC_COMMAND)
 
         self.subproc = None
 
     def handle_red_button(self):
-        print('Button!')
+        """Stop all audio and reset the field to 'breathe'"""
+        # field_config = {"id":4,"fx":2,"sx":52,"ix": 128, "bri":255,
+        #                 "pal":0,"col":[[255,0,0],[0,0,0],[85,0,255]]}
+        self.light_board.set_field(LightBoard.FIELD_BREATHE)
+        self.light_board.push_state()
 
-        # Terminate the running process if one is running
-        if self.subproc is not None:
+        if ( self.check_process() != AudioState.STOPPED and
+             self.subproc is not None ):
             self.terminate_process()
+            self.audio_state = AudioState.STOPPED
+
+    def handle_blue_button(self):
+        print('Blue button')
+        started = self.start_or_stop(AudioState.MR_ROBOTO, MR_ROBOTO_COMMAND)
+
+        # field_config = {"id":4,"fx":48,"sx":66,"ix":192,"bri":128,
+        #                 "pal":5,"col":[[255,0,0],[255,166,0],[0,255,21]]}
+        if started:
+            self.light_board.set_field(LightBoard.FIELD_MUSIC)
         else:
-            self.start_process(MR_ROBOTO_COMMAND)
+            self.light_board.set_field(LightBoard.FIELD_BREATHE)
+        self.light_board.push_state()
 
     def handle_yellow_button(self):
-        print('Yellow Button!')
+        print('Yellow button')
+        self.start_or_stop(AudioState.TALKING, TALKING_COMMAND)
+
+    def handle_black_button(self):
+        print('Black button')
+        self.start_or_stop(AudioState.FARTING, FARTING_COMMAND())
+
+    def handle_r_white_button(self):
+        """Play the walking sound on a loop."""
+        print('R White Button')
+        self.start_or_stop(AudioState.WALKING, WALKING_COMMAND)
+
+    def handle_l_white_button(self):
+        print('L White Button')
+        self.start_or_stop(AudioState.THINKING, THINKING_COMMAND)
+
+    def handle_green_button(self):
+        print('Green Button')
+        started = self.start_or_stop(AudioState.THRILLER, THRILLER_COMMAND)
+
+        if started:
+            self.light_board.set_field(LightBoard.FIELD_MUSIC)
+        else:
+            self.light_board.set_field(LightBoard.FIELD_BREATHE)
+        self.light_board.push_state()
+
+    def headlights(self):
+        """It's dark outside and it's hard to see.  Turn on some light."""
+        self.light_board.headlights()
+
+    def start_or_stop(self, action: AudioState, command: list[str]) -> bool:
+        should_start = True
 
         # Terminate the running process if one is running
-        if self.subproc is not None:
+        if ( self.check_process() != AudioState.STOPPED and
+             self.subproc is not None ):
+            if self.audio_state == action:
+                should_start = False
             self.terminate_process()
-        else:
-            self.start_process(MR_ROBOTO_COMMAND)
+            self.audio_state = AudioState.STOPPED
+        if should_start:
+            self.audio_state = action
+            self.start_process(command)
+
+        return should_start
+
 
 def main():
-    robo = Robocalypse(red_button_gpio = RED_BUTTON_GPIO,
-                       yellow_button_gpio = YELLOW_BUTTON_GPIO)
+
+    robo = Robocalypse(gpios = ALL_BUTTONS)
 
     robo.go()
     # # Create the a button object with a pull up resistor in place.
